24dc9ac5bdfb84d10d97b7a3ee664adebbf4fe40
Miguel Valero <m.valero@tomorrowlabs.io>
2019-11-28 20:57:30 +0100

    Unify date expectations

--- tst/Serialize/JsonSerializeTest.cs
+++ tst/Serialize/JsonSerializeTest.cs
@@ -17,14 +17,15 @@ namespace Tlabs.Data.Serialize.Tests {
     [Fact]
     public void BasicJsonSerializeTest() {
       IDynamicSerializer ser= Json.JsonFormat.CreateDynSerializer();
+
       var strm= new MemoryStream();
-      var d0= new DateTime(2018,12,01,9,0,0);
-      dynamic obj= new {
+      var d0=  DateTime.Parse("2018-12-01T09:00:00.000000Z", null, System.Globalization.DateTimeStyles.RoundtripKind);
+      dynamic originalObj= new {
         StrProp= "abc",
         NumProp= 2.718281828,
         DateProp= Tlabs.App.TimeInfo.ToAppTime(d0)
       };
-      ser.WriteObj(strm, obj);
+      ser.WriteObj(strm, originalObj);
       strm.Position= 0;
       var json= Encoding.UTF8.GetString(strm.ToArray());
       Assert.Contains("\"strProp\"", json);
@@ -33,34 +34,35 @@ namespace Tlabs.Data.Serialize.Tests {
       // Returns UTC
       Assert.Contains("2018-12-01T09:00:00.0000000Z", json);
 
-      dynamic obj2= ser.LoadObj(strm, ((object)obj).GetType());
-      Assert.Equal(obj.StrProp, obj2.StrProp);
-      Assert.Equal(obj.NumProp, obj2.NumProp);
-      Assert.Equal(obj.DateProp, obj2.DateProp);
+      dynamic deserializedObj= ser.LoadObj(strm, ((object)originalObj).GetType());
+      Assert.Equal(originalObj.StrProp, deserializedObj.StrProp);
+      Assert.Equal(originalObj.NumProp, deserializedObj.NumProp);
+      Assert.Equal(originalObj.DateProp, deserializedObj.DateProp);
 
       // De-serializes from UTC in application time zone
-      dynamic obj3= ser.LoadObj("{\"dateProp\": \"1996-12-19T16:39:57.0000000Z\"}", ((object)obj).GetType());
-      var d= Tlabs.App.TimeInfo.ToAppTime(new DateTime(1996, 12, 19, 16, 39, 57));
-      Assert.Equal(d, obj3.DateProp);
+      dynamic deserializedFromUtc= ser.LoadObj("{\"dateProp\": \"1996-12-19T16:39:57.0000000Z\"}", ((object)originalObj).GetType());
+      var d=  DateTime.Parse("1996-12-19T17:39:57.000000Z", null, System.Globalization.DateTimeStyles.RoundtripKind);
+      Assert.Equal(d, deserializedFromUtc.DateProp);
 
-      // Serializes it back correctly into UTC
+      // Serializes it back again into UTC TZ
       strm= new MemoryStream();
-      ser.WriteObj(strm, obj3);
+      ser.WriteObj(strm, deserializedFromUtc);
       strm.Position= 0;
       json= Encoding.UTF8.GetString(strm.ToArray());
       Assert.Contains("1996-12-19T16:39:57.0000000Z", json);
 
       // De-serializes from -06:00 in application time zone
-      dynamic obj5= ser.LoadObj("{\"dateProp\": \"1996-12-19T16:39:57.000000-06:00\"}", ((object)obj).GetType());
-      var d1= Tlabs.App.TimeInfo.ToAppTime(new DateTime(1996, 12, 19, 22, 39, 57));
-      Assert.Equal(d1, obj5.DateProp);
+      dynamic deserializedFromTZ= ser.LoadObj("{\"dateProp\": \"1996-12-19T16:39:57.000000-06:00\"}", ((object)originalObj).GetType());
+      var d4=  DateTime.Parse("1996-12-19T22:39:57.000000Z", null, System.Globalization.DateTimeStyles.RoundtripKind);
+      Assert.Equal(App.TimeInfo.ToAppTime(d4), deserializedFromTZ.DateProp);
 
       // Allows timestamp
-      dynamic obj4= ser.LoadObj("{\"dateProp\": 1563197860271}", ((object)obj).GetType());
-      Assert.Equal(Tlabs.App.TimeInfo.ToAppTime(new DateTime(1996, 12, 19, 16, 39, 57)), obj3.DateProp);
+      dynamic deserializedFromTS= ser.LoadObj("{\"dateProp\": 1563197860271}", ((object)originalObj).GetType());
+      var d5=  DateTime.Parse("2019-07-15T13:37:40.2710000Z", null, System.Globalization.DateTimeStyles.RoundtripKind);
+      Assert.Equal(Tlabs.App.TimeInfo.ToAppTime(d5), deserializedFromTS.DateProp);
 
       // Rejects only date
-      Assert.Throws<JsonSerializationException>(() => ser.LoadObj("{\"dateProp\": \"2019-20-10\"}", ((object)obj).GetType()));
+      Assert.Throws<JsonSerializationException>(() => ser.LoadObj("{\"dateProp\": \"2019-20-10\"}", ((object)originalObj).GetType()));
     }
 
   }
