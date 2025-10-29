762701723a146972e5539f57ef531d8fb7107dea
Dmitrii Shepilov <d.shepilov@tomorrowlabs.io>
2025-10-29 11:21:01 +0100

    Autofill Created field on insert

--- src/Entity/Intern/DocumentEntity.cs
+++ src/Entity/Intern/DocumentEntity.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using Tlabs.Data.Event;
 
-namespace Tlabs.Data.Entity.Intern {
+namespace Tlabs.Data.Entity.Intern
+{
   /// <summary>
   /// Document entity interface
   /// </summary>
-  public interface IDocumentEntity {
+  public interface IDocumentEntity
+  {
     /// <summary>
     /// Creation timestamp
     /// </summary>
@@ -19,22 +22,46 @@ namespace Tlabs.Data.Entity.Intern {
   /// <summary>
   /// Base document entity which stores a series of 
   /// </summary>
-  public abstract class DocumentEntity : BaseEntity, IDocumentEntity {
+  public abstract class DocumentEntity : BaseEntity, IDocumentEntity
+  {
     /// <inheritdoc/>
     public DateTime Created { get; set; }
 
     /// <inheritdoc/>
     public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
+
+    static DocumentEntity()
+    {
+      DataStoreEvent<DocumentEntity>.Inserting += setCreated;
+    }
+
+    private static void setCreated(Event.IEvent<DocumentEntity> ev)
+    {
+      DocumentEntity ent = ev.Entity;
+      ent.Created = App.TimeInfo.Now;
+    }
   }
 
   /// <summary>
   /// Base document entity which stores a series of 
   /// </summary>
-  public abstract class EditableDocumentEntity : EditableEntity, IDocumentEntity {
+  public abstract class EditableDocumentEntity : EditableEntity, IDocumentEntity
+  {
     /// <inheritdoc/>
     public DateTime Created { get; set; }
 
     /// <inheritdoc/>
     public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
+
+    static EditableDocumentEntity()
+    {
+      DataStoreEvent<EditableDocumentEntity>.Inserting += setCreated;
+    }
+
+    private static void setCreated(Event.IEvent<EditableDocumentEntity> ev)
+    {
+      EditableDocumentEntity ent = ev.Entity;
+      ent.Created = App.TimeInfo.Now;
+    }
   }
 }
