82e570ba43e0c8015317088874d263915243deee
Dmitrii Shepilov <d.shepilov@tomorrowlabs.io>
2025-10-30 10:11:00 +0100

    Move the Created DateTime to Editable entity, DcumentEntity to inherit from EditableEntity

--- src/Entity/Intern/DocumentEntity.cs
+++ src/Entity/Intern/DocumentEntity.cs
@@ -10,10 +10,6 @@ namespace Tlabs.Data.Entity.Intern
   public interface IDocumentEntity
   {
     /// <summary>
-    /// Creation timestamp
-    /// </summary>
-    public DateTime Created { get; set; }
-    /// <summary>
     /// Serialized 
     /// </summary>
     public IDictionary<string, object> Properties { get; set; }
@@ -22,24 +18,10 @@ namespace Tlabs.Data.Entity.Intern
   /// <summary>
   /// Base document entity which stores a series of 
   /// </summary>
-  public abstract class DocumentEntity : BaseEntity, IDocumentEntity
+  public abstract class DocumentEntity : EditableEntity, IDocumentEntity
   {
     /// <inheritdoc/>
-    public DateTime Created { get; set; }
-
-    /// <inheritdoc/>
     public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
-
-    static DocumentEntity()
-    {
-      DataStoreEvent<DocumentEntity>.Inserting += setCreated;
-    }
-
-    private static void setCreated(Event.IEvent<DocumentEntity> ev)
-    {
-      DocumentEntity ent = ev.Entity;
-      ent.Created = App.TimeInfo.Now;
-    }
   }
 
   /// <summary>
@@ -48,20 +30,6 @@ namespace Tlabs.Data.Entity.Intern
   public abstract class EditableDocumentEntity : EditableEntity, IDocumentEntity
   {
     /// <inheritdoc/>
-    public DateTime Created { get; set; }
-
-    /// <inheritdoc/>
     public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
-
-    static EditableDocumentEntity()
-    {
-      DataStoreEvent<EditableDocumentEntity>.Inserting += setCreated;
-    }
-
-    private static void setCreated(Event.IEvent<EditableDocumentEntity> ev)
-    {
-      EditableDocumentEntity ent = ev.Entity;
-      ent.Created = App.TimeInfo.Now;
-    }
   }
 }
--- src/Entity/Intern/EditableEntity.cs
+++ src/Entity/Intern/EditableEntity.cs
@@ -8,17 +8,27 @@ namespace Tlabs.Data.Entity.Intern {
 
   ///<summary>Common base of an editiable entity.</summary>
   public abstract class EditableEntity : BaseEntity {
+    ///<summary>Creation date.</summary>
+    public DateTime Created { get; set; }
 
     static EditableEntity() {
-      DataStoreEvent<EditableEntity>.Inserting+= setModified;
-      DataStoreEvent<EditableEntity>.Updating+= setModified;
+      DataStoreEvent<EditableEntity>.Inserting += setCreated;
+      DataStoreEvent<EditableEntity>.Inserting += setModified;
+      DataStoreEvent<EditableEntity>.Updating += setModified;
     }
 
-    private static void setModified(Event.IEvent<EditableEntity> ev) {
-      EditableEntity ent= ev.Entity;
-      ent.Modified= App.TimeInfo.Now;
-      var usr= (Identity.IIdentityAccessor?)App.ServiceProv.GetService<Identity.IIdentityAccessor>();
-      ent.Editor= string.IsNullOrEmpty(usr?.Name) ? "Anonymous" : usr.AuthenticationType + "/" + usr.Name; //Identity.IIdentityAccessor service could be missing
+    private static void setModified(Event.IEvent<EditableEntity> ev)
+    {
+      EditableEntity ent = ev.Entity;
+      ent.Modified = App.TimeInfo.Now;
+      var usr = (Identity.IIdentityAccessor?)App.ServiceProv.GetService<Identity.IIdentityAccessor>();
+      ent.Editor = string.IsNullOrEmpty(usr?.Name) ? "Anonymous" : usr.AuthenticationType + "/" + usr.Name; //Identity.IIdentityAccessor service could be missing
+    }
+    
+    private static void setCreated(Event.IEvent<EditableEntity> ev)
+    {
+      EditableEntity ent = ev.Entity;
+      ent.Created = App.TimeInfo.Now;
     }
 
     ///<summary>Editor user identifier.</summary>
